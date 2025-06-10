8b8ddd89e7980771478b4e935271a2d242871ad7
kuatov2002 <channelgamer58@gmail.com>
2025-06-10 21:43:56 +0500

    player rigidbody to controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,39 +3,41 @@ using Mirror;
 using UnityEngine;
 using Unity.Cinemachine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : NetworkBehaviour
 {
     [Header("Настройки движения")]
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.81f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform followTarget;
-    [Header("Камера FreeLook")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
     
-
+    [Header("Камера FreeLook")]
     [SerializeField] private Gun gun;
-
-    private Rigidbody _rb;
+    
+    private CharacterController _controller;
+    private Vector3 _velocity;
     private bool _isGrounded;
     private CinemachineCamera[] freeLookCam;
     private Vector2 _look;
+
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
+        _controller = GetComponent<CharacterController>();
     }
 
     public override void OnStartLocalPlayer()
     {
         if (!isLocalPlayer) return;
-
-
+        
         freeLookCam = FindObjectsOfType<CinemachineCamera>();
         freeLookCam = freeLookCam
             .OrderByDescending(cam => cam.Priority.Value)
             .ToArray();
-        
-        
+
         foreach (var cam in freeLookCam)
         {
             if (cam)
@@ -50,69 +52,95 @@ public class PlayerMovement : NetworkBehaviour
     {
         if (!isLocalPlayer) return;
 
+        HandleGroundCheck();
+        HandleJump();
+        HandleShooting();
+        HandleMouseLook();
+        HandleMovement();
+    }
+
+    private void HandleGroundCheck()
+    {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+    }
+
+    private void HandleJump()
+    {
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
-            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            _isGrounded = false;
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+    }
 
+    private void HandleShooting()
+    {
         if (Input.GetButtonDown("Fire1"))
         {
-            if (gun.Charge()) freeLookCam[0].gameObject.SetActive(false);
+            if (gun.Charge()) 
+                freeLookCam[0].gameObject.SetActive(false);
         }
 
         if (Input.GetButtonUp("Fire1"))
         {
-            if (gun.Fire()) freeLookCam[0].gameObject.SetActive(true);
+            if (gun.Fire()) 
+                freeLookCam[0].gameObject.SetActive(true);
         }
+    }
 
+    private void HandleMouseLook()
+    {
         _look.x = Input.GetAxis("Mouse X");
         _look.y = -Input.GetAxis("Mouse Y");
-        followTarget.rotation *= Quaternion.AngleAxis(_look.x,Vector3.up);
-        followTarget.rotation *= Quaternion.AngleAxis(_look.y,Vector3.right);
+
+        followTarget.rotation *= Quaternion.AngleAxis(_look.x, Vector3.up);
+        followTarget.rotation *= Quaternion.AngleAxis(_look.y, Vector3.right);
 
         var angles = followTarget.localEulerAngles;
         angles.z = 0;
 
         var angle = followTarget.localEulerAngles.x;
-
         if (angle > 180 && angle < 280)
         {
             angles.x = 300;
-        }else if (angle < 180 && angle > 70)
+        }
+        else if (angle < 180 && angle > 70)
         {
             angles.x = 70;
         }
 
         followTarget.localEulerAngles = angles;
-
         transform.rotation = Quaternion.Euler(0, followTarget.rotation.eulerAngles.y, 0);
         followTarget.localEulerAngles = new Vector3(angles.x, 0, 0);
-        
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 inputVector = new Vector3(horizontal, 0f, vertical).normalized;
-    
-        if (inputVector.magnitude > 0.01f)
-        {
-            Vector3 moveDirection = transform.TransformDirection(inputVector);
-            Vector3 targetVelocity = moveDirection * moveSpeed;
-            _rb.linearVelocity = new Vector3(targetVelocity.x, _rb.linearVelocity.y, targetVelocity.z);
-        }
-        else
-        {
-            _rb.linearVelocity = new Vector3(0, _rb.linearVelocity.y, 0);
-        }
     }
 
-    private void FixedUpdate()
+    private void HandleMovement()
     {
-        if (!isLocalPlayer) return;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 inputVector = new Vector3(horizontal, 0f, vertical).normalized;
 
+        Vector3 moveDirection = Vector3.zero;
+        if (inputVector.magnitude > 0.01f)
+        {
+            moveDirection = transform.TransformDirection(inputVector) * moveSpeed;
+        }
 
+        _velocity.y += gravity * Time.deltaTime;
+        Vector3 finalMovement = moveDirection + Vector3.up * _velocity.y;
+        _controller.Move(finalMovement * Time.deltaTime);
+    }
 
-        _isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.2f, groundLayer);
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = _isGrounded ? Color.green : Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+        }
     }
 }
