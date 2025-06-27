b0a9fe3994878193c79bc55edf38d939d7b9dd5a
kuatov2002 <channelgamer58@gmail.com>
2025-06-27 14:41:31 +0500

    negr fix
    пофикшен баг когда негру не давали золота и пули после попадания по нему

--- Assets/Scripts/Characters/Shadow/ShadowHealth.cs
+++ Assets/Scripts/Characters/Shadow/ShadowHealth.cs
@@ -1,5 +1,6 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShadowHealth : PlayerHealth
 {
@@ -7,16 +8,15 @@ public class ShadowHealth : PlayerHealth
     
     [SyncVar]
     private bool isInvisible = false;
-
     private MeshRenderer meshRenderer;
     private Material originalMaterial;
     private Material invisibleMaterial;
-
+    
     private void Start()
     {
         InitializeMaterials();
     }
-
+    
     private void InitializeMaterials()
     {
         meshRenderer = GetComponent<MeshRenderer>();
@@ -26,13 +26,13 @@ public class ShadowHealth : PlayerHealth
             invisibleMaterial = new Material(originalMaterial);
         }
     }
-
+    
     public override void TakeDamage(float damage)
     {
         base.TakeDamage(damage);
         ActivateInvisibility();
     }
-
+    
     [Server]
     private void ActivateInvisibility()
     {
@@ -40,7 +40,7 @@ public class ShadowHealth : PlayerHealth
         RpcHideShadow();
         Invoke(nameof(DeactivateInvisibility), invisibilityDuration);
     }
-
+    
     [Server]
     private void DeactivateInvisibility()
     {
@@ -53,31 +53,30 @@ public class ShadowHealth : PlayerHealth
     {
         if (!isLocalPlayer && !isServerOnly)
         {
-            gameObject.SetActive(false);
+            SetVisibility(false);
         }
         else
         {
             SetMaterialAlpha(0f);
         }
     }
-
+    
     [ClientRpc]
     private void RpcShowShadow()
     {
         if (!isLocalPlayer && !isServerOnly)
         {
-            gameObject.SetActive(true);
+            SetVisibility(true);
         }
         else
         {
             SetMaterialAlpha(1f);
         }
     }
-
+    
     private void SetMaterialAlpha(float alpha)
     {
         if (!meshRenderer) return;
-
         Material targetMaterial = alpha == 0f ? invisibleMaterial : originalMaterial;
         
         if (targetMaterial.HasProperty("_BaseMap"))
@@ -89,7 +88,31 @@ public class ShadowHealth : PlayerHealth
         
         meshRenderer.material = targetMaterial;
     }
-
+    
+    private void SetVisibility(bool visible)
+    {
+        // Отключение всех Renderer компонентов
+        var renderers = GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            renderer.enabled = visible;
+        }
+        
+        // Отключение всех UI компонентов
+        var graphicComponents = GetComponentsInChildren<Graphic>();
+        foreach (var graphic in graphicComponents)
+        {
+            graphic.enabled = visible;
+        }
+        
+        // Альтернативно, можно отключить Canvas компоненты
+        var canvasComponents = GetComponentsInChildren<Canvas>();
+        foreach (var canvas in canvasComponents)
+        {
+            canvas.enabled = visible;
+        }
+    }
+    
     private void OnDestroy()
     {
         if (invisibleMaterial)
